327a5bb5c4b60a87ce7e4d3a01719e90aed7d2ce
Costin Didoaca <didoacacostin@yahoo.com>
2024-07-11 17:30:12 +0300

    Implemented news endpoint

--- app/views.py
+++ app/views.py
@@ -8,7 +8,7 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
+from app.models import News
 from datetime import datetime, timedelta
 from itertools import groupby
 
@@ -617,14 +617,49 @@ class UploadStudentsExcelView(APIView):
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-
+################################ NEWS VIEW
 class NewsView(generics.ListAPIView):
     serializer_class = NewsSerializer
+    
+    def post(self, request, *args, **kwargs):
+        serializer = NewsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def get(self, request, *args, **kwargs):
+        school_id = kwargs.get("school_id")
+        student_id = kwargs.get("student_id")
+            
+        if not school_id:
+            return Response({"error": "school_id required"}, status=status.HTTP_400_BAD_REQUEST)
+
+        if student_id:
+            news_queryset_by_student = NewsService.get_news_by_student_and_school(school_id, student_id)
+        
+        serializer = NewsSerializer(news_queryset_by_student, many=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def delete(self, request, *args, **kwargs):
+            news_id = kwargs.get("news_id")
+            
+            if not news_id:
+                return Response({"error": "news_id required"}, status=status.HTTP_400_BAD_REQUEST)
+            
+            try:
+                news = NewsService.get_news_by_news_id(news_id)
+            except News.DoesNotExist:
+                return Response({"error": "News not found"}, status=status.HTTP_404_NOT_FOUND)
+            
+            news.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
 
     def get_queryset(self):
         return NewsService.get_news_by_school(self.request.user.user_school.all().first())
 
-################################################# SCHOOL CALENDAR
+################################################# SCHOOL CALENDAR VIEW
 class SchoolCalendarView(APIView):
     permission_classes = [IsAuthenticated]
 
