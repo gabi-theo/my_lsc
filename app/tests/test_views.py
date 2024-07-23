fe9cd4e1324a98f22a4fd1e1c51e26fe5966bcdc
Gabi <g.isaila@enbw.com>
2024-07-23 20:12:02 +0300

    Fixed news url

---
+++ app/tests/test_views.py
@@ -0,0 +1,8 @@
+
+
+from django.test import TestCase
+
+
+class TestViews(TestCase):
+    def test_1():
+        assert 1==1
--- app/urls/school.py
+++ app/urls/school.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path("days_off/", DaysOffListView.as_view()),
     path("rooms/", RoomListCreateView.as_view()),
     path("school_create/", SchoolCreateView.as_view()),
-    path("news/", NewsView.as_view()),
+    path("news/<uuid:school_id>/<uuid:student_id>/", NewsView.as_view()),
     path("school_schedules/<uuid:school_id>/",
          DailySchoolScheduleAPIView.as_view()),
 ]
