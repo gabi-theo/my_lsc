6e3f82c718ace5ef9b191156f15da45b9a02fcfd
gabi-theo <52572318+gabi-theo@users.noreply.github.com>
2024-08-08 17:19:05 +0300

    Update feedback.py

--- app/services/feedback.py
+++ app/services/feedback.py
@@ -6,5 +6,3 @@ class FeedbackService:
     @staticmethod
     def get_feedback_by_id(feedback_id):
         return get_object_or_404(Feedback, pk=feedback_id)
-
-# docker exec -it my_lsc-db-1 psql -U myuser -d myapp -c "DROP SCHEMA public CASCADE; CREATE SCHEMA public;"
